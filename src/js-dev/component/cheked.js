f21a971325bbaef112106f4f9a4c5319c1ee1cc1
18320867985 <huangqishanyx@163.com>
2018-01-22 18:55:39 +0800

    2018-1-22

---
+++ src/js-dev/component/cheked.js
@@ -0,0 +1,22 @@
++ function() {
+
+// 给checkbox 自动添加boolean 值
+//	$("input[type=checkbox]").on("click",function() {
+//		var bl = $(this).attr("checked");
+//		if(typeof bl !== "undefined") {
+//			$(this).triggerHandler("click");
+//			$(this).attr("checked", true);
+//		} else {
+//			//$(this).triggerHandler("click");
+//				//$(this).attr("checked", false);
+//			$(this).removeAttr("checked");
+//		}
+//
+//	});
+}()
+
+
+
+
+
+
--- src/js/all.js
+++ src/js/all.js
@@ -12266,6 +12266,22 @@ var wxLazy = function ($) {
 		}
 	});
 }(window.jQuery || window.Zepto);
++function () {
+
+  // 给checkbox 自动添加boolean 值
+  //	$("input[type=checkbox]").on("click",function() {
+  //		var bl = $(this).attr("checked");
+  //		if(typeof bl !== "undefined") {
+  //			$(this).triggerHandler("click");
+  //			$(this).attr("checked", true);
+  //		} else {
+  //			//$(this).triggerHandler("click");
+  //				//$(this).attr("checked", false);
+  //			$(this).removeAttr("checked");
+  //		}
+  //
+  //	});
+}();
 mui.init({
 
 	swipeBack: true //启用右滑关闭功能
